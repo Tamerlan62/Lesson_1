575306a460fee12fb4f7c7ab9d001a9068ce9bba
Tamerlan <tamerlan.recebov062@gmail.com>
2024-01-26 23:51:31 +0400

    add lesson1;task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 17;
+int b = 1;
+int c = 85;
+int d = 38;
+int e = 98;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
